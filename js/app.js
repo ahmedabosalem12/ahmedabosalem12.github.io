ff6ff6c00e1d7913e840bfb5a904801a6bedc279
atosa12 <93831545+atosa12@users.noreply.github.com>
2023-02-19 23:21:08 +0200

    speedup

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 var i = 0;
 var txt = 'Welcome to Enactus cic!'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var speed = 5; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
