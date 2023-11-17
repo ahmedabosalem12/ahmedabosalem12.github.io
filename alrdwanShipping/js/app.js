4bbfe128615945cabd90e9d3254b3b5ff5d7d557
ahmed salem <93831545+ahmedabosalem12@users.noreply.github.com>
2023-11-17 15:34:04 +0200

    tslem

--- alrdwanShipping/js/app.js
+++ alrdwanShipping/js/app.js
@@ -39,6 +39,27 @@ function toggleReadMore() {
   }
 }
 
+let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        const slides = document.getElementsByClassName("mySlides");
+        
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        slideIndex++;
+        
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 2000); // Change slide every 2 seconds
+    }
+
 /**
  * header
  */
