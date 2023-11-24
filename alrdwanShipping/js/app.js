5f2a3c8621909868bd83e76c790bf25f65c92a23
ahmed salem <93831545+ahmedabosalem12@users.noreply.github.com>
2023-11-24 20:07:46 +0200

    all

--- alrdwanShipping/js/app.js
+++ alrdwanShipping/js/app.js
@@ -111,3 +111,49 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+function expandImage(image) {
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+
+  const expandedImage = new Image();
+  expandedImage.src = image.src;
+  expandedImage.classList.add('expanded-image');
+
+  overlay.appendChild(expandedImage);
+  document.body.appendChild(overlay);
+
+  overlay.addEventListener('click', () => {
+    overlay.remove();
+  });
+}
+
+function expandVideo(video) {
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+
+  const expandedVideo = document.createElement('video');
+  expandedVideo.src = video.src;
+  expandedVideo.controls = true;
+  expandedVideo.classList.add('expanded-video');
+
+  overlay.appendChild(expandedVideo);
+  document.body.appendChild(overlay);}
+  var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
