351b4d4ac56d06ff15ad149b6227b32a27dd0837
ahmed salem <93831545+ahmedabosalem12@users.noreply.github.com>
2023-12-18 10:10:01 +0200

    add

--- alrdwanShipping/app.js
+++ alrdwanShipping/app.js
@@ -201,3 +201,11 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+document.getElementById('next').onclick = function(){
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+  let lists = document.querySelectorAll('.item');
+  document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
