154582ba7b84e47c03f6952b7c06ef2e03a5d39f
ahmed salem <93831545+ahmedabosalem12@users.noreply.github.com>
2024-01-02 16:54:45 +0200

    ro7 warah

--- alrdwanShipping/app.js
+++ alrdwanShipping/app.js
@@ -291,3 +291,18 @@ var customSlideIndex = 1;
     slides[customSlideIndex - 1].style.display = "block";
   }
 
+  $(document).ready(function() {
+
+    $('.counter').each(function () {
+$(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+}, {
+    duration: 4000,
+    easing: 'swing',
+    step: function (now) {
+        $(this).text(Math.ceil(now));
+    }
+});
+}); 
+
+});  
