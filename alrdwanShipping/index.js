48da00fbc4093b49159cd4c85e46c84cc0259791
ahmed salem <93831545+ahmedabosalem12@users.noreply.github.com>
2024-01-02 21:12:34 +0200

    arf3

--- alrdwanShipping/index.js
+++ alrdwanShipping/index.js
@@ -21,4 +21,23 @@ function sendMail() {
   
   }
   
+  const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < navToggler.length; i++) {
+  navToggler[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+}
+
   
