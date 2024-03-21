e13a006b273efa93d71a4c1dfab80f5353159056
ahmed salem <93831545+ahmedabosalem12@users.noreply.github.com>
2024-03-21 22:03:07 +0200

    shipp

--- AR/js/main.js
+++ AR/js/main.js
@@ -107,3 +107,26 @@
     
 })(jQuery);
 
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_9nw6clm";
+    const templateID = "template_zsljwgy";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
